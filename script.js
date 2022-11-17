95694e04d262bb21130c24386bbff5644d03d5ff
Glitch (a-frame-final-eduardo-medina) <none>
2022-11-17 05:49:25 +0000

    ‼️🌠 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,138 @@
+ function camone1() {
+        document.querySelector("#cam").emit("movecam_1");
+        console.log("movecam_1");
+      }
+
+ function camone2() {
+        document.querySelector("#cam").emit("movecam_2");
+        console.log("movecam_2");
+      }
+
+ function camone3() {
+        document.querySelector("#cam").emit("movecam_3");
+        console.log("movecam_3");
+      }
+
+ function camone4() {
+        document.querySelector("#cam").emit("movecam_4");
+        console.log("movecam_4");
+      }
+
+ function camone5() {
+        document.querySelector("#cam").emit("movecam_5");
+        console.log("movecam_5");
+      }
+
+ function camone6() {
+        document.querySelector("#cam").emit("movecam_6");
+        console.log("movecam_6");
+      }
+
+ function camone7() {
+        document.querySelector("#cam").emit("movecam_7");
+        console.log("movecam_7");
+      }
+
+ function result1_1() {
+        document.querySelector("#cam").emit("movecam_11");
+        console.log("movecam_11");
+      }
+
+ function result1_2() {
+        document.querySelector("#cam").emit("movecam_12");
+        console.log("movecam_12");
+      }
+
+ function result1_3() {
+        document.querySelector("#cam").emit("movecam_13");
+        console.log("movecam_13");
+      }
+
+ function result1_4() {
+        document.querySelector("#cam").emit("movecam_14");
+        console.log("movecam_14");
+      }
+
+ function result2_1() {
+        document.querySelector("#cam").emit("movecam_21");
+        console.log("movecam_21");
+      }
+
+ function result2_2() {
+        document.querySelector("#cam").emit("movecam_22");
+        console.log("movecam_22");
+      }
+
+ function result2_3() {
+        document.querySelector("#cam").emit("movecam_23");
+        console.log("movecam_23");
+      }
+
+ function result2_4() {
+        document.querySelector("#cam").emit("movecam_24");
+        console.log("movecam_24");
+      }
+
+ function result3_1() {
+        document.querySelector("#cam").emit("movecam_31");
+        console.log("movecam_31");
+      }
+
+ function result3_2() {
+        document.querySelector("#cam").emit("movecam_32");
+        console.log("movecam_32");
+      }
+
+ function result4_1() {
+        document.querySelector("#cam").emit("movecam_41");
+        console.log("movecam_41");
+      }
+
+ function result4_2() {
+        document.querySelector("#cam").emit("movecam_42");
+        console.log("movecam_42");
+      }
+
+ function result4_3() {
+        document.querySelector("#cam").emit("movecam_43");
+        console.log("movecam_43");
+      }
+
+ function result5_1() {
+        document.querySelector("#cam").emit("movecam_51");
+        console.log("movecam_51");
+      }
+
+ function result5_2() {
+        document.querySelector("#cam").emit("movecam_52");
+        console.log("movecam_52");
+      }
+
+ function result5_3() {
+        document.querySelector("#cam").emit("movecam_53");
+        console.log("movecam_53");
+      }
+
+ function result6_1() {
+        document.querySelector("#cam").emit("movecam_61");
+        console.log("movecam_61");
+      }
+
+ function result6_2() {
+        document.querySelector("#cam").emit("movecam_62");
+        console.log("movecam_62");
+      }
+
+ function result6_3() {
+        document.querySelector("#cam").emit("movecam_63");
+        console.log("movecam_63");
+      }
+
+ function result7_1() {
+        document.querySelector("#cam").emit("movecam_71");
+        console.log("movecam_71");
+      }
+
+// $("#noBoxOne").click(function(){
+//   $("#oneNo").hide();
+// });
